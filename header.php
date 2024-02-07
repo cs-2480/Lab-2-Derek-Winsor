6e953b0b01de0cb46343321712bf67dc9e75206a
dpwinsor <91507440+dpwinsor@users.noreply.github.com>
2024-02-07 13:11:08 -0500

    Lab message: 2 Created header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+
+$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+
+$path_parts = pathinfo($phpSelf);
+
+?>
+<main>
+    <header>
+        <h1>Plastic Waste is Littering Our Planet</h1>
+    </header>
