b549068172bd39c72aec7aa2990ee6ea20c35bcb
dpwinsor <91507440+dpwinsor@users.noreply.github.com>
2024-02-07 13:11:51 -0500

    Lab message: 2 Created footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+
+$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+
+$path_parts = pathinfo($phpSelf);
+
+?>
+
+            <footer>
+                <h3>Sources</h3>
+                <cite>
+                    <a href = "https://blog.arcadiapower.com/15-key-facts-statistics-about-plastic-pollution/" >Blog.ArcadiaPower</a>
+                    <a href = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiP6ZDh0sfkAhUwUt8KHctJBK8Qjhx6BAgBEAI&url=https%3A%2F%2Fnewrepublic.com%2Farticle%2F147988%2Fglobal-crisis-plastic-pollution&psig=AOvVaw0DWVqQlf4tywH3uufmO4hM&ust=1568252669741368">Img source</a>
+                    <a href = "https://www.pewtrusts.org/en/research-and-analysis/articles/2018/09/24/plastic-pollution-affects-sea-life-throughout-the-ocean">Pewtrusts.org</a>
+                    <a href = "https://www.nationalgeographic.org/encyclopedia/great-pacific-garbage-patch/" >National geographic</a>
+                    <a href = "https://marinedebris.noaa.gov/info/patch.html">Marine debris</a>
+                    <a href = "https://www.greenpeace.org/usa/oceans/preventing-plastic-pollution/" >Green peace</a>
+                    <a href = "https://www.earthday.org/2018/06/08/what-you-can-do-to-end-plastic-pollution/">Earth day</a>
+                    <a href = "https://www.greenpeace.org/international/press-release/18872/coca-cola-pepsico-and-nestle-found-to-be-worst-plastic-polluters-worldwide-in-global-cleanups-and-brand-audits/">Image source</a>
+            
+                </cite>
+
+                <p>By Derek Winsor</p>
+                <p>winsor.derek@gmail.com</p>
+            </footer>
+        </main>
+</body>
+</html>
+
