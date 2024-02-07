0e300b30e12706b47b9ddee98596d830ddd2d1b3
dpwinsor <91507440+dpwinsor@users.noreply.github.com>
2024-02-07 13:27:51 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,4 @@
-<?php
 
-$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
-
-$path_parts = pathinfo($phpSelf);
-
-?>
 <main>
     <header>
         <h1>Plastic Waste is Littering Our Planet</h1>
